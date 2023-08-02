6b3d953d265e5c0b2eb0fb31234c14ed38774867
Fuda van Diggelen <fuda.van.diggelen@vu.nl>
2023-08-02 17:28:14 +0200

    depth prediction dataset enrichment

--- init_exp.bash
+++ init_exp.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 PYTHON_VIRTUAL_ENV_PATH=$(which python)
+PYTHON_VIRTUAL_ENV_PATH=${PYTHON_VIRTUAL_ENV_PATH%/bin/*}
+HOST_NAME=${PYTHON_VIRTUAL_ENV_PATH#*venv_}
 LD_PATHS=LD_LIBRARY_PATH:
 
-for library in $(ls -R "${PYTHON_VIRTUAL_ENV_PATH%bin/*}lib/python3.8/site-packages/nvidia/" | grep lib:)
+for library in $(ls -R "${PYTHON_VIRTUAL_ENV_PATH%/bin/*}/lib/python3.8/site-packages/nvidia/" | grep lib:)
   do
   LD_PATHS=$LD_PATHS$library
   done
@@ -12,7 +14,12 @@ export LD_LIBRARY_PATH=$LD_PATHS
 
 cd planner_learning
 
+#start_time=$(date +"%H:%M:%S")
+#screen -d -m -S "${start_time}" -L -Logfile "./train_${HOST_NAME}_${start_time}.log" env LD_LIBRARY_PATH=$LD_PATHS nice -n0 python "train.py" --settings_file="config/train_settings_${HOST_NAME}.yaml" & pids+=($!)
+#wait "${pids[@]}"
+#cd ../
+
 start_time=$(date +"%H:%M:%S")
-screen -d -m -S "${start_time}" -L -Logfile "./train_${start_time}.log" env LD_LIBRARY_PATH=$LD_PATHS nice -n0 python "train.py" --settings_file="config/train_settings.yaml" & pids+=($!)
+screen -d -m -S "${start_time}" -L -Logfile "./train_${HOST_NAME}_${start_time}.log" env LD_LIBRARY_PATH=$LD_PATHS nice -n0 python "train.py" --settings_file="config/train_settings_${HOST_NAME}.yaml" & pids+=($!)
 wait "${pids[@]}"
 cd ../
